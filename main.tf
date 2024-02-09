ea7b84300371fe32292e8033e314dfa4d231188c
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2024-02-10 00:04:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,24 @@ data "template_file" "user_data_test" {
       }
 }
 
+data "aws_ami" "amazon_linux_2" {
+ most_recent = true
+
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
+
+ filter {
+   name = "architecture"
+   values = ["x86_64"]
+ }
+
+ owners = ["137112412989"] #amazon
+}
+
 resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
+  ami           = "${data.aws_ami.amazon_linux_2.id}"
   instance_type = "t2.micro"
   user_data = "${data.template_file.user_data_test.rendered}"
 
